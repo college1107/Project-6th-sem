af9c6a0c4c995131b9957172bf49ba950a088217
YASH CHAUDHARI <Yashchaudhari0604@gmail.com>
2023-12-19 23:26:57 +0530

    DAY 1 done
    first of all check static file path is is correct for model or not i'm not sure about that so first check it and check their path in our registration.html file css linkage (i will not load static files globally so check on according to that), secondly check the url folder of user , and finally run the code...
    
    and and and  important vat....  kai error lage to jate sudhari levi, baki kai mathakut na karavi , lagu pade vat thi
    
    ###sher ne DAY 1 ka code complete kiya  🦁🦁🦁🦁😎😎

--- Attendance_System/User/views.py
+++ Attendance_System/User/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def U_home(request):
     return render(request,'U_index.html')
+
+def register(request):
+    return render (request,'registration.html')
