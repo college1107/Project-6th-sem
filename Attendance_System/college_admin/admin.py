0a1bde3fc942fe3b2d5f647c82c7c9c43a9850fb
YASH CHAUDHARI <Yashchaudhari0604@gmail.com>
2023-12-21 22:31:32 +0530

    day 2
    done

--- Attendance_System/college_admin/admin.py
+++ Attendance_System/college_admin/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from college_admin.models import *
 # Register your models here.
-admin.site.register(register)
+
+
+class regAdmin(admin.ModelAdmin):
+    list_display =  ('en_no','name','img')
+
+admin.site.register(register,regAdmin)
