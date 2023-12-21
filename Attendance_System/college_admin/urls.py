281bbfcfd87e30344e9db2b764096f9791f12787
om <152455978+college1107@users.noreply.github.com>
2023-12-21 09:01:17 +0530

    commit

--- Attendance_System/college_admin/urls.py
+++ Attendance_System/college_admin/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from college_admin.views import *
 urlpatterns = [
     path('',CA_home,name='CA_home'),
+    path('empty_database/', empty_database, name='empty_database'),
 ]
--- Attendance_System/college_admin/views.py
+++ Attendance_System/college_admin/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from college_admin.utils import *
 from django.contrib import messages
+from college_admin.models import *
 
 
 def inserting(enno, name, image):
@@ -9,17 +10,38 @@ def inserting(enno, name, image):
         return True
 
 
+def empty_database(request):
+    context = {"color": "success"}
+    try:
+        register.objects.all().delete()  # Replace 'YourModel' with the actual model you want to clear
+        messages.success(request, "Database cleared successfully.")
+    except Exception as e:
+        messages.error(request, f"Error clearing database: {str(e)}")
+    # render(request, "CA_index.html",context)
+    return redirect("CA_home")
+
+
 # Create your views here.
 def CA_home(request):
     context = {
         "page": "Admin",
     }
+
     if request.method == "POST":
         data = request.POST
         name = data.get("name")
         en_no = data.get("en_no")
         img = request.FILES.get("image")
-        if inserting(en_no, name, img) :
+
+        if register.objects.filter(en_no=en_no).exists():
+            messages.success(request, "Same Enrollment number in DB")
+            context.update({"color": "danger"})
+            return render(request, "CA_index.html", context)
+
+        if inserting(en_no, name, img):
             messages.success(request, "Data Submitted")
             context.update({"color": "success"})
-    return render(request, "CA_index.html",context)
+
+    # Ensure that "page" is still in the context, even after POST requests
+    return render(request, "CA_index.html", context)
+
