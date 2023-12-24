d617f320937512f8b6e49b592b83bcb34d992da8
om <ldrpcollage@gmail.com>
2023-12-24 07:51:06 +0530

    commit

--- Attendance_System/college_admin/utils.py
+++ Attendance_System/college_admin/utils.py
@@ -5,26 +5,17 @@ def Insert(en_no, name, image):
     a = register.objects.create(en_no=en_no, name=name, img=image)
     return a
 
-import os
+def Emptying():
+    current_directory = os.getcwd()
+    file_path = os.path.join(current_directory,'img')
+    print(file_path)
+    try:
+        if os.path.exists(file_path):
+            for img in os.listdir(file_path):
+                os.remove(f"{file_path}/{img}")
+            print(f"File {file_path} removed successfully.")
+        else:
+            print(f"File {file_path} does not exist.")
+    except Exception as e:
+        print(f"Error removing file {file_path}: {e}")
 
-def Emptying(path):
-    cwd = os.getcwd()
-    abs_path = os.path.abspath(path)
-    abs_path = abs_path.replace("\\", "/")
-    print(os.listdir(abs_path))
-    for i in os.listdir(abs_path):
-        file_to_remove = os.path.join(abs_path, i)
-        os.remove(file_to_remove)
-
-
-
-
-
-# # Get the current working directory
-
-# # Define the relative path to the file you want to remove
-
-# # Construct the absolute path to the file
-
-# # Remove the file
-# os.remove(abs_path)
