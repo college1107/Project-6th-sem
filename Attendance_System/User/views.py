1956c7e1bb0aae2afd756ad81f4e68e5ac1564a7
om <152455978+college1107@users.noreply.github.com>
2024-01-04 11:54:07 +0530

    commit

--- Attendance_System/User/views.py
+++ Attendance_System/User/views.py
@@ -3,10 +3,29 @@ from django.contrib import messages
 from college_admin.models import register
 from User.models import attending_class
 from User.utils import Insert
-
+from ipware import get_client_ip
+import requests
 
 # Create your views here.
 def U_home(request):
+    client_ip = request.META.get('REMOTE_ADDR')
+    if client_ip:
+        # Make a request to the IP geolocation API
+        access_key = '17bbc22a35afbb5ad7ebce4fe74b56c2'
+        api_url = f'http://api.ipstack.com/{client_ip}?access_key={access_key}'
+        
+        # Perform the API request
+        response = requests.get(api_url)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the response JSON
+            geolocation_data = response.json()
+            print(f"Client's IP address: {client_ip}, Geolocation Data: {geolocation_data}")
+        else:
+           print(f"Failed to fetch geolocation data. Status Code: {response.status_code}")
+    else:
+        print("Unable to get the client's IP address")
     context = {"page": "Attendance", "color": "info"}
     if request.method == "POST":
         data = request.POST
