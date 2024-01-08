ae9780e54cd41f713570c3d160b9cac00697e67d
om <ldrpcollage@gmail.com>
2024-01-08 07:12:13 +0530

    c

--- Attendance_System/User/views.py
+++ Attendance_System/User/views.py
@@ -3,22 +3,28 @@ from django.contrib import messages
 from college_admin.models import register
 from User.models import attending_class
 from User.utils import Insert
-
+import base64
 
 # Create your views here.
 def U_home(request):
+    img=''
     context = {"page": "Attendance", "color": "info"}
     if request.method == "POST":
         image_data = request.POST.get("image_data")
         print(f'the image is {image_data}')
-        with open('img.jpg','wb') as f:
-            f.write(image_data)
+        if image_data is None:
+            pass
+        else:
+            img=image_data
         en_no = str(request.POST.get("en_no"))
         en_no=en_no.upper()
-        if en_no == "" and image_data is None:
+        if en_no == "" and image_data == 'None':
             messages.success(request, "Missing Field's")
             context.update({"color": "danger"})
             return render(request, "U_index.html", context)
+        image_data = base64.b64decode(img)
+        with open('img.jpg', 'wb') as f:
+            f.write(image_data)
         if register.objects.filter(en_no=en_no).exists():
             if register.objects.filter(en_no=en_no, attended=False).exists():
                 Insert(en_no)
@@ -31,7 +37,6 @@ def U_home(request):
             messages.success(request, "Not registered in DB")
             context.update({"color": "danger"})
             return render(request, "U_index.html", context)
-
     return render(request, "U_index.html", context)
 
 
